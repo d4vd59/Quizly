3f485b4acb27af68ddaffa901ef1a1c40ebee8a2
N4E3O0 <neo.nettersheim@edu.bib.de>
2026-02-04 11:56:22 +0100

    Klassen Quizly

---
+++ Frage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizly
+{
+    public class Frage
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        public Kategorie Kategorie { get; set; }
+        public Schwierigkeitsgrad Schwierigkeitsgrad { get; set; }
+
+        public int RichtigBeantwortet { get; set; }
+        public int FalschBeantwortet { get; set; }
+
+        public List<Antwort> Antworten { get; set; }
+
+        public bool IstAntwortRichtig(int antwortId)
+        {
+            return Antworten.Any(a => a.Id == antwortId && a.IstRichtig);
+        }
+    }
+}
---
+++ Spieler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizly
+{
+    public class Spieler
+    {
+        public int UserId { get; set; }
+        public string Nickname { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public DateTime? LastLogin { get; set; }
+
+        public int GesamtPunkte { get; set; }
+
+        public void AddPunkte(int punkte)
+        {
+            GesamtPunkte += punkte;
+        }
+    }
+}
